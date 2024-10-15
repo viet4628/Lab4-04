31eae6905df02d75ed333c9b6754230e52492951
Viet Xuan Nguyen <Viet Xuan Nguyen@DESKTOP-0MEO1CU>
2024-10-16 01:19:50 +0700

    Add project files.

---
+++ Lab4-04/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Lab4_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimeStart = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeEnd = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkMonth = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.dgvOderBill = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOderBill)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimeStart);
+            this.groupBox1.Controls.Add(this.dateTimeEnd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkMonth);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(677, 106);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Đơn Hàng";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(285, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "~";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dateTimeStart
+            // 
+            this.dateTimeStart.CustomFormat = "dd/MM/yyyy";
+            this.dateTimeStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimeStart.Location = new System.Drawing.Point(148, 61);
+            this.dateTimeStart.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimeStart.Name = "dateTimeStart";
+            this.dateTimeStart.Size = new System.Drawing.Size(122, 20);
+            this.dateTimeStart.TabIndex = 4;
+            this.dateTimeStart.Value = new System.DateTime(2024, 10, 15, 20, 20, 24, 0);
+            this.dateTimeStart.ValueChanged += new System.EventHandler(this.dateTimeStart_ValueChanged);
+            // 
+            // dateTimeEnd
+            // 
+            this.dateTimeEnd.CustomFormat = "dd/MM/yyyy";
+            this.dateTimeEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimeEnd.Location = new System.Drawing.Point(317, 61);
+            this.dateTimeEnd.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimeEnd.Name = "dateTimeEnd";
+            this.dateTimeEnd.Size = new System.Drawing.Size(111, 20);
+            this.dateTimeEnd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thời Gian Giao Hàng";
+            // 
+            // checkMonth
+            // 
+            this.checkMonth.AutoSize = true;
+            this.checkMonth.Location = new System.Drawing.Point(34, 20);
+            this.checkMonth.Name = "checkMonth";
+            this.checkMonth.Size = new System.Drawing.Size(134, 17);
+            this.checkMonth.TabIndex = 0;
+            this.checkMonth.Text = "Xem tất cả trong tháng";
+            this.checkMonth.UseVisualStyleBackColor = true;
+            this.checkMonth.CheckedChanged += new System.EventHandler(this.checkMonth_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(550, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tổng Cộng";
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(617, 425);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(75, 20);
+            this.txtSum.TabIndex = 7;
+            this.txtSum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dgvOderBill
+            // 
+            this.dgvOderBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOderBill.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvOderBill.Location = new System.Drawing.Point(12, 134);
+            this.dgvOderBill.Name = "dgvOderBill";
+            this.dgvOderBill.Size = new System.Drawing.Size(680, 285);
+            this.dgvOderBill.TabIndex = 8;
+            this.dgvOderBill.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "STT";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 45;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Số HĐ";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Ngày Đặt Hàng";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Ngày Giao Hàng";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Thành Tiền";
+            this.Column5.Name = "Column5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.dgvOderBill);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = " Quản Lý Đơn Hàng Và Bán Hàng";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOderBill)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimeStart;
+        private System.Windows.Forms.DateTimePicker dateTimeEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.DataGridView dgvOderBill;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Lab4-04/Form1.cs
@@ -0,0 +1,128 @@
+﻿using DevExpress.Data.Browsing;
+using Lab4_04.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_04
+{
+    public partial class Form1 : Form
+    {
+        private int sothutu = 1;
+        QuanLyBanHangDB context = new QuanLyBanHangDB();
+        public Form1()
+        {
+            InitializeComponent();
+            this.dateTimeEnd.ValueChanged += new EventHandler(dateTimeStart_ValueChanged);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void FillBill(List<Invoice> listInvoice)
+        {
+            dgvOderBill.Rows.Clear();
+            foreach (var item in listInvoice)
+            {
+                int index = dgvOderBill.Rows.Add();
+                dgvOderBill.Rows[index].Cells[0].Value = sothutu;
+                dgvOderBill.Rows[index].Cells[1].Value = item.InvoiceNo;
+                dgvOderBill.Rows[index].Cells[2].Value = item.OrderDate;
+                dgvOderBill.Rows[index].Cells[3].Value = item.DeliveryDate;
+                dgvOderBill.Rows[index].Cells[4].Value = item.Orders.Sum(s => s.Quantity*s.Price);
+                sothutu++;
+            }
+        }
+        private void dateTimeDefault()
+        {
+            dateTimeStart.Value = new DateTime(2019, 10, 1);
+            dateTimeEnd.Value = new DateTime(2019, 10, 31);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                txtSum.Text = "0";
+                dateTimeDefault();
+                List<Invoice> listInvoice = context.Invoices.ToList();
+                FillBill(listInvoice);
+                CountDgv();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void CountDgv()
+        {
+            int sumSoluong = 0;
+            for (int i = 0; i < dgvOderBill.Rows.Count; i++)
+            {
+                if (dgvOderBill.Rows[i].Cells[0].Value != null)
+                {
+                    sumSoluong++;
+                }
+            }
+            txtSum.Text = sumSoluong.ToString();
+        }
+
+        private void checkMonth_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkMonth.Checked)
+            {
+                DateTime today = DateTime.Today;
+                DateTime firstDayofMonth = new DateTime(today.Year, today.Month, 1);
+                DateTime lastDayofMonth = new DateTime(today.Year, today.Month, DateTime.DaysInMonth(today.Year, today.Month));
+                List<Invoice> fillInvoice = context.Invoices.ToList()
+                    .Where(f => f.OrderDate >= firstDayofMonth && f.DeliveryDate <= lastDayofMonth).ToList();
+                FillBill(fillInvoice);
+                CountDgv();
+                sothutu = 1;
+            }
+            else
+            {
+                Form1_Load(sender, e);
+            }
+        }
+
+        private void dateTimeStart_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dateTimeStart.Value > dateTimeEnd.Value)
+                {
+                    dateTimeEnd.Value = dateTimeStart.Value;
+                }
+                else
+                {
+                    List<Invoice> fillInvoice = context.Invoices.ToList().Where(f => f.DeliveryDate >= dateTimeStart.Value && f.DeliveryDate <= dateTimeEnd.Value).ToList();
+                    FillBill(fillInvoice);
+                    CountDgv();
+                    sothutu = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
